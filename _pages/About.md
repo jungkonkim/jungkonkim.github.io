45243af3225c1ed3832233b3dcd8f67c5f24bace
jungkonkim <jungkon117@gmail.com>
2020-02-22 17:00:24 +0900

    base8

--- _pages/About.md
+++ _pages/About.md
@@ -6,4 +6,4 @@ layout: single
 
 ## jungkonkim.github.io 블로그
 
-이 블로그는 GitHub Pages 블로그 서비스인 github.io를 같이 만들어보는 샘플 블로그이다.s
+이 블로그는 GitHub Pages 블로그 서비스인 github.io를 같이 만들어보는 샘플 블로그이다.  
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,11 @@
 var store = [{
         "title": "github.io 블로그 시작하기",
         "excerpt":"GitHub Blog 서비스인 github.io 블로그 시작하기로 했다. GitHub Blog 서비스의 이름은 Pages이다. Pages가 다른 블로그 플랫폼 보다 편한 것 같아서 마음에 든다. 다른 사람들도 같이 많이 사용했으면 좋겠다는 생각이 든다. YFM에서 정의한 제목을 이중 괄호 구문으로 본문에 추가할 수 있다. 이 글의 제목은 github.io 블로그 시작하기이고 마지막으로 수정된 시간은 2020-02-22...","categories": ["Blog"],
-        "tags": ["Blog"],
+        "tags": ["tag1","tag2","tag3"],
         "url": "http://localhost:4000/blog/first-post/",
+        "teaser":null},{
+        "title": "github.io 블로그 시작하기2",
+        "excerpt":"GitHub Blog 서비스인 github.io 블로그 시작하기로 했다. GitHub Blog 서비스의 이름은 Pages이다. Pages가 다른 블로그 플랫폼 보다 편한 것 같아서 마음에 든다. 다른 사람들도 같이 많이 사용했으면 좋겠다는 생각이 든다. YFM에서 정의한 제목을 이중 괄호 구문으로 본문에 추가할 수 있다. 이 글의 제목은 github.io 블로그 시작하기2이고 마지막으로 수정된 시간은 2020-02-22...","categories": ["Blog"],
+        "tags": ["tag2","tag3","tag4"],
+        "url": "http://localhost:4000/blog/second-post/",
         "teaser":null}]
